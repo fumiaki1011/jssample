560ecfbebc67a9ecc9c403eabe149a06a5830c9c
fk <f.jissyou@gmail.com>
2020-05-18 08:01:39 +0000

    kakurituhenkou

--- js/main.js
+++ js/main.js
@@ -6,17 +6,17 @@
   btn.addEventListener('click', () =>{
 
     const n = Math.random();
-    if (n < 0.15){
+    if (n < 0.05){
       btn.textContent = '大吉';
+    }else if (n < 0.15){
+      btn.textContent = '中吉';
     }else if (n < 0.25){
       btn.textContent = '小吉';
     }else if (n < 0.45){
-      btn.textContent = '中吉';
-    }else if (n < 0.6){
       btn.textContent = '末吉';
     }else if (n < 0.65){
       btn.textContent = '吉';
-    }else if (n < 0.8){
+    }else if (n < 0.9){
       btn.textContent = '凶';
     }else{
       btn.textContent = '大凶';
