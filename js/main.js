95d3c2278c199745f0da7a6ef7c4664ed12492b1
fk <f.jissyou@gmail.com>
2020-05-18 04:38:06 +0000

    add new file

---
+++ js/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+  
+  btn.addEventListener('click', () =>{
+
+    const n = Math.random();
+    if (n < 0.15){
+      btn.textContent = '大吉';
+    }else if (n < 0.25){
+      btn.textContent = '小吉';
+    }else if (n < 0.45){
+      btn.textContent = '中吉';
+    }else if (n < 0.6){
+      btn.textContent = '末吉';
+    }else if (n < 0.65){
+      btn.textContent = '吉';
+    }else if (n < 0.8){
+      btn.textContent = '凶';
+    }else{
+      btn.textContent = '大凶';
+    }
+  });
+}
